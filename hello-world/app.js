cf753d2377e96eedbce39ad486c9b65d33f720c6
mmdagdelen <mustafa-_-dagdelen@hotmail.com.tr>
2017-03-08 21:18:37 +0300

    initial commit

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,17 +1,14 @@
 var http = require("http");
 
-http.createServer(function(req,resp){
-    if(req.url=="/")
-    {
-        resp.end("Anasayfa");
+http.createServer(function (req, resp) {
+    if (req.url == "/") {
+        resp.end("<b>Anasayfa</b>");
     }
-    else if(req.url=="/about")
-    {
-        resp.end("Hakkımızda");        
+    else if (req.url == "/about") {
+        resp.end("<i>Hakkımızda</i>");
     }
-    else    
-            resp.end("Diğer request");        
+    else
+        resp.end("<u>Diğer request</u>");
 
 }).listen("3000");
 
-console.log("Server started");

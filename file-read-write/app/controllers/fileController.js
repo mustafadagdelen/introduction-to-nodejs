f57b779c2bbd72e633c9c52df6761316bd43edf5
mmdagdelen <mustafa-_-dagdelen@hotmail.com.tr>
2017-03-08 21:20:29 +0300

    empty folder removed

--- file-read-write/app/controllers/fileController.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = (app, config) => {
-    return (req, res, next) => {
-
-
-    }
-};
--- file-read-write/config/config.js
+++
@@ -1,8 +0,0 @@
-var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
-
-var config = require('./configRaw.json');
-config[env].root = rootPath;
-
-module.exports = config[env];
